5dddaf5207898d2f9b9dc8f3b5f1019c89139812
Jennifer11607 <jenniferjimenez152000@gmail.com>
2024-06-10 08:03:56 -0600

    cambio de fuente en vistas especialidades

--- vistas/especialidades/buscar.php
+++ vistas/especialidades/buscar.php
@@ -5,22 +5,23 @@ error_reporting(E_ALL);
 
 include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
-    <div class="container">
-        <h1 class="text-center">BUSCAR ESPECIALIDADES</h1>
-        <div class="row justify-content-center">
-            <form action="/hospital_final_jimenez/controladores/especialidades/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
-                <div class="row mb-3">
-                    <div class="col">
-                        <label for="espec_nombre">Especialidad</label>
-                        <input type="text" name="espec_nombre" id="espec_nombre" class="form-control">
-                    </div>
+
+<div class="container full-height d-flex flex-column justify-content-center align-items-center">
+        <div class="w-25 text-center mb-4 mt-5" style="font-family:'Courier New', Courier, monospace">
+            <h1>BUSCAR ESPECIALIDADES</h1>
+        </div>
+        <div class="row justify-content-center w-25">
+            <form action="/hospital_final_jimenez/controladores/especialidades/buscar.php" method="GET" class="form-container border p-4 rounded" style="font-family:'Courier New', Courier, monospace; background-color: #FFE082">
+                <div class="form-group mb-3">
+                    <label for="espec_nombre">Especialidad</label>
+                    <input type="text" name="espec_nombre" id="espec_nombre" class="form-control">
                 </div>
-                <div class="row mb-3">
-                    <div class="col">
-                        <button type="submit" class="btn btn-info w-100">Buscar</button>
-                    </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-info btn-small">Buscar</button>
                 </div>
             </form>
         </div>
     </div>
+
+
     <?php include_once '../../includes/footer.php'?>
--- vistas/especialidades/index.php
+++ vistas/especialidades/index.php
@@ -5,19 +5,17 @@
 include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
         <div class="container mt-5" style="max-width: 700px; background-color: #2471A3; border: solid 2px; border-color: #fff;">
-        <h1 class="text-center"style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">INGRESO ESPECIALIDADES</h1>
+        <h1 class="text-center"style="font-family:'Courier New', Courier, monospace">INGRESO ESPECIALIDADES</h1>
         <div class="row justify-content-center">
-            <form action="/hospital_final_jimenez/controladores/especialidades/guardar.php" method="POST" class="col-lg-8 border  p-3 mb-4" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; background-color:#AAB7B8;">
+            <form action="/hospital_final_jimenez/controladores/especialidades/guardar.php" method="POST" class="col-lg-8 border  p-3 mb-4" style="font-family: 'Courier New', Courier, monospace; background-color:#AAB7B8;">
                 <div class="row mb-3">
                     <div class="col">
                     <label for="espec_nombre">Nombre de la Especialidad</label>
                         <input type="text" name="espec_nombre" id="espec_nombre" class="form-control">
                     </div>
                 </div>
-                <div class="row mb-3">
-                    <div class="col">
-                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
-                    </div>
+                <div class="button-container text-center">
+                        <button type="submit" class="btn btn-primary btn-small">Guardar</button>
                 </div>
             </form>
         </div>
