334b0d4fbff8335e283ced81ad34e993549dbf00
Jennifer11607 <jenniferjimenez152000@gmail.com>
2024-06-10 00:27:00 -0600

    agregando estilo a buscar paciente

--- vistas/pacientes/buscar.php
+++ vistas/pacientes/buscar.php
@@ -4,28 +4,26 @@
 //error_reporting(E_ALL); ?>
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
-    <div class="container ">
-    <h1 class="text-center" style="font-family: cursive;">Buscar Pacientes</h1>
+
+
+<div class="container  mt-5 ">
+        <h1 class="text-center mb-4" style="font-family:'Courier New', Courier, monospace">Buscar Pacientes</h1>
         <div class="row justify-content-center">
-            <form action="/hospital_final_jimenez/controladores/pacientes/buscar.php" method="GET" class="col-lg-8 border bg-light p-3" style="font-family: cursive;">
-                <div class="row mb-3">
-                    <div class="col">
-                        <label for="paciente_nombre">Nombre del paciente</label>
-                        <input type="text" name="paciente_nombre" id="paciente_nombre" class="form-control">
-                    </div>
+            <form action="/hospital_final_jimenez/controladores/pacientes/buscar.php" method="GET" class="col-lg-6 border  p-3 rounded" style="font-family:'Courier New', Courier, monospace; background-color: #F0B27A ">
+                <div class="form-group">
+                    <label for="paciente_nombre">Nombre del paciente</label>
+                    <input type="text" name="paciente_nombre" id="paciente_nombre" class="form-control">
                 </div>
-                <div class="row mb-3">
-                    <div class="col">
-                        <label for="paciente_dpi">No. de DPI</label>
-                        <input type="text" name="paciente_dpi" id="paciente_dpi" class="form-control">
-                    </div>
+                <div class="form-group mb-3">
+                    <label for="paciente_dpi">No. de DPI</label>
+                    <input type="text" name="paciente_dpi" id="paciente_dpi" class="form-control">
                 </div>
-                <div class="row mb-3">
-                    <div class="col">
-                        <button type="submit" class="btn btn-info w-100">Buscar</button>
-                    </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-info btn-sm">Buscar</button>
                 </div>
             </form>
         </div>
     </div>
+
+
 <?php include_once '../../includes/footer.php'?>
